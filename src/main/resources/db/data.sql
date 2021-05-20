1829785be4e4abbd929a82b3aa217a92c97bb243
Just1lz <exelent.pride@rambler.ru>
2021-05-20 13:25:07 +0700

    4.7 Домашняя работа - корректировка

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -27,6 +27,6 @@ insert into books (author, title, priceOld, price) values (1, 'Java HARDCODE', '
 -- select * from books
 -- left join authors a on a.id = books.author;
 -- select * from authors where category = 'Б';
-SELECT * FROM books left join authors a on a.id = books.author where books.title like '%' || 'real' || '%';
-SELECT * FROM books left join authors a on a.id = books.author where lastname like '%' || 'han' || '%';
-SELECT * FROM books left join authors a on a.id = books.author;
+-- SELECT * FROM books left join authors a on a.id = books.author where books.title like '%' || 'real' || '%';
+-- SELECT * FROM books left join authors a on a.id = books.author where lastname like '%' || 'han' || '%';
+-- SELECT * FROM books left join authors a on a.id = books.author;
